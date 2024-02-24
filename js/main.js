45932553d64a2cea84a6566adbaf6bea6950a7d0
zkim <zandergunn@gmail.com>
2024-02-23 17:53:16 -0800

    inital commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+  
+
+// mastermind and blackjack but wanted to create a game that utilized a card deck
+
+
+  /*----- constants -----*/
+
+  // card deck: 52 cards per deck, have dealer cycle through 3 decks and then shuffle
+
+  /*----- state variables -----*/
+
+  // gameboard "table" = 2 card spaces for player and dealer
+  // turn: player goes first, then runs DealerTurn
+  // winner: winner, tie, house wins, or bust
+
+
+  /*----- cached elements  -----*/
+// messages
+// play again button
+
+  /*----- event listeners -----*/
+// click buttons -> hit, stand, bet, SPLIT optional
+// playAgain button
+
+  /*----- functions -----*/
+
+  // playGame () / renderGame ()
+ // init()
+
+ // function playGame -> 
+ //       startGame, 
+ //             playerBet
+//                  dealPlayerHand, 
+//                         if hand = 21 -> stand
+//                          SPLIT optional (part of both player and house)
+//                   dealHouseHand
+//                        if hand >= 17 -> stay
+//                findWinner() -> Player wins/House wins/Push/
+//              nextHand with same deck or recycling one 52-card deck
+
+// ADDITIONAL flavor
+// different table colors
+// different decks
+// input user name, choose signature affectation/style
+// sound effects -- stars/sparkles when you hit blackjack
+  //  reshuffling the deck, dealing out cards, dealing out chips
+// visual effects -- fireworks on blackjack
+
